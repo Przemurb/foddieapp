8f146c0c30540feabd2dede946d7514a541a41a0
Przemurb <przemurb@hotmail.it>
2022-05-05 14:14:01 +0200

    dodano dane testowe

--- src/main/java/com/example/foddieapp/Order.java
+++ src/main/java/com/example/foddieapp/Order.java
@@ -17,7 +17,10 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "order_item",
+            joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"))
     private List<Item> items = new ArrayList<>();
     private String address;
     private String telephone;
