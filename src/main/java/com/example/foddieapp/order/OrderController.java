4c201ffb63db22b453fd19e341719e9ef701b386
Przemurb <przemurb@hotmail.it>
2022-07-06 13:05:49 +0200

    rozbudowano kontroler zamówień zmapowano obsługę zamówień utworzono stronę order.html

--- src/main/java/com/example/foddieapp/order/OrderController.java
+++ src/main/java/com/example/foddieapp/order/OrderController.java
@@ -12,25 +12,40 @@ import java.util.Optional;
 
 @Controller
 public class OrderController {
-    private ClientOrder clientOrder;
-    private ItemRepository repository;
+    private final ClientOrder clientOrder;
+    private final ItemRepository itemRepository;
+    private final OrderRepository orderRepository;
 
-    public OrderController(ClientOrder clientOrder, ItemRepository repository) {
+    public OrderController(ClientOrder clientOrder, ItemRepository itemRepository, OrderRepository orderRepository) {
         this.clientOrder = clientOrder;
-        this.repository = repository;
+        this.itemRepository = itemRepository;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping("/zamowienie/dodaj")
     public String addItemToOrder (@RequestParam Long itemId, Model model) {
-        Optional<Item> foundItem = repository.findById(itemId);
+        Optional<Item> foundItem = itemRepository.findById(itemId);
         foundItem.ifPresent(clientOrder::add);
         if(foundItem.isPresent()) {
-            model.addAttribute("message", new Message("Dodano", "Do zamówienia dodano: " +
+            model.addAttribute("message", new Message("Dodano do zamówienia", "Do zamówienia dodano: " +
                     foundItem.get().getName()));
         } else {
             model.addAttribute("message", new Message("Nic nie dodano", "Z menu pobrano błędne id " +
                     itemId));
         }
         return "message";
+    }
+
+    @GetMapping("/zamowienie")
+    public String getCurrentOrder(Model model) {
+        model.addAttribute("order", clientOrder.getOrder());
+        model.addAttribute("sum", clientOrder
+                .getOrder()
+                .getItems()
+                .stream()
+                .mapToDouble(Item::getPrice)
+                .sum());
+
+        return "order";
     }
 }
