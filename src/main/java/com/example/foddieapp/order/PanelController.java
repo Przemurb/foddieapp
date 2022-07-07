7509fd6fbbdeb1ef272d4d719c77a689cd1a29ac
Przemurb <przemurb@hotmail.it>
2022-07-07 12:50:54 +0200

    dodano kontroler panela zamówień dodano stronę panela zamówień

---
+++ src/main/java/com/example/foddieapp/order/PanelController.java
@@ -0,0 +1,29 @@
+package com.example.foddieapp.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class PanelController {
+    private OrderRepository orderRepository;
+
+    public PanelController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/panel/zamowienia")
+    public String getOrders(@RequestParam(required = false) OrderStatus status, Model model) {
+        List<Order> orders;
+        if(status == null) {
+            orders = orderRepository.findAll();
+        } else {
+            orders = orderRepository.findAllByStatus(status);
+        }
+        model.addAttribute("orders", orders);
+        return "panel/orders";
+    }
+}
